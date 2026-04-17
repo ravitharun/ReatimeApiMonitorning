6860f4a657b9260cf9b8a3a03764bfa1b181498f
Ravi Tharun <tr565003@gmail.com>
2026-04-17 08:05:49 +0530

    added the toast for the api status testing

--- server/config/Db.js
+++ server/config/Db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const URI =
-  process.env.NODE_ENV === "production"
-    ? process.env.MONGO_URI
-    : process.env.Local_MONGO_URI;
-    console.log(URI,'URI');
+// const URI =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.MONGO_URI
+//     : process.env.Local_MONGO_URI;
+//     console.log(URI,'URI');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/ApiMonitoring');
--- server/controllers/check.js
+++ server/controllers/check.js
@@ -5,8 +5,15 @@ const check = async (req, res) => {
     try {
         const io = getIO();
         const { Apilogs } = req.body;
+        console.log(Apilogs.status =='SUCCESS','Apilogs')
+        if (Apilogs.status =='SUCCESS') {
+
+             io.emit("CheckLogsNotif", Apilogs)
+             return "email notification to be sent"
+
+        }
         console.log(Apilogs, 'logs from port 3000.')
-        const saveLogs = await  ApiLogs.create(Apilogs)
+        const saveLogs = await ApiLogs.create(Apilogs)
         console.log(saveLogs, 'logs info Form DB');
 
         return res.status(200).json({
